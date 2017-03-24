0cc151244a3e859aebd40ddb3387657ab2ba8c7a
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-03-24 12:40:47 -0700

    revised branching

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,36 @@ $(document).ready(function(){
 
     $(".csharp, .ruby, .php").hide()
     var education = $("#eduacation").val();
-    var location = $("location").val();
-    var start = $("start").val();
+    var location = $("#location").val();
+    var start = $("#start").val();
     var size = $("#size").val();
     var design = $("#design").val();
-    var build = $("build").val();
+    var build = $("#build").val();
 
     if (size === "Established company" && design === "Design/interaction"){
       $(".csharp").show();
     }
-    else if (size === "Start-up" && design === "Behind the Scenes"){
-      $(".ruby").show();
-    }
-    else if (size === "Either" && design === "Either"){
+    else if (design === "Behind the Scenes" || "Either" && design !== "Design/interaction"){
       $(".php").show();
     }
-    else if (start === "April or May" && size === "Established company" && design === "Design/interaction"){
-      $(".csharp").show();
-    } 
+    else if(start === "April or May" || "June or June" || "Either"){
+      $(".ruby").show();
+    }
+
+    // else if (start == "April or May" && size === "Start-up")
+    //   $(".ruby").show();
+    //
+    // // else if ((start === "April or May") && (size === "Established company" || "Start-up" || "Either") && (design === "Design/interaction" || "Behind the Scenes" || "Either")){
+    // //   $(".ruby").show();
+    // // }
+    // else if (size === "Either" && design === "Either" || "Behind the Scenes"){
+    //   $(".php").show();
+    // }
+    // else if (start === "April or May" && size === "Established company" && design === "Design/interaction"){
+    //   $(".csharp").show();
+    // }
+
+
 
 
 
