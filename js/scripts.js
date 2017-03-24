de3a1e0bc2507f9ed4192b998a880fe235422729
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-03-24 10:27:58 -0700

    add five questions and make vars for them

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ $(document).ready(function(){
   $("#language").submit(function(event){
 
     $(".csharp, .ruby, .php").hide()
+    var education = $("#eduacation").val();
+    var location = $("location").val();
     var size = $("#size").val();
     var design = $("#design").val();
-
+    var build = $("build").val();
 
     if (size === "Established company" && design === "Design/interaction"){
       $(".csharp").show();
+    }
+    if (size === "Start-up" && design === "Behind the Scenes"){
+      $(".ruby").show();
     }
 
 
